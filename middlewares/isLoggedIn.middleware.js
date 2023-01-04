6ceb368d75189d1f542169dd048b1bdbeecaf1b6
yaman1337 <yaman.sarabariya@gmail.com>
2023-01-04 12:12:00 +0545

    bug fix

--- middlewares/isLoggedIn.middleware.js
+++ middlewares/isLoggedIn.middleware.js
@@ -10,7 +10,7 @@ const errors = {
 export default async function isLoggedIn(req, res, next) {
   const { authorization } = req.headers;
 
-  let token = authorization.split(" ")[1];
+  let token =  authorization && authorization.split(" ")[1];
 
   if (!token) {
     return res
