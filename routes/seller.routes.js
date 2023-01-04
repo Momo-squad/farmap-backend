e0089bdf3dd780648f5fe6da42df729e1c434865
yaman1337 <yaman.sarabariya@gmail.com>
2023-01-04 14:52:37 +0545

    implement checkrole middleware on each request

--- routes/seller.routes.js
+++ routes/seller.routes.js
@@ -1,10 +1,12 @@
 import express from "express"
 import { getProfileData } from "../controllers/seller/index.js";
 import isLoggedIn from "../middlewares/isLoggedIn.middleware.js";
+import checkRoles from "../middlewares/checkRoles.middleware.js"
 
 const sellerRouter = express.Router()
 
 sellerRouter.use(isLoggedIn)
+sellerRouter.use(checkRoles.isSeller)
 
 sellerRouter
     .get("/profile", getProfileData)
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,12 +1,13 @@
-import express from "express"
+import express from "express";
 import { getProfileData } from "../controllers/user/index.js";
 import isLoggedIn from "../middlewares/isLoggedIn.middleware.js";
+import checkRoles from "../middlewares/checkRoles.middleware.js";
 
-const userRouter = express.Router()
+const userRouter = express.Router();
 
-userRouter.use(isLoggedIn)
+userRouter.use(isLoggedIn);
+userRouter.use(checkRoles.isUser);
 
-userRouter
-    .get("/profile", getProfileData)
+userRouter.get("/profile", getProfileData);
 
 export default userRouter;
