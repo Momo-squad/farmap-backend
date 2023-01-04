a9a367ece02c733b69d20ca53178851c37a32a12
yaman1337 <yaman.sarabariya@gmail.com>
2023-01-04 14:52:59 +0545

    add get profile controller

--- controllers/user/getProfileData.controller.js
+++ controllers/user/getProfileData.controller.js
@@ -1,7 +1,14 @@
 import asyncHandler from "../../utils/asyncHandler.js";
+import User from "../../models/User.js";
 
 const getProfileData = asyncHandler(async (req, res) => {
-    res.send("get profile data")
+    const data = await User.findOne({ email: req.user.email, role: req.user.role, username: req.user.username }).select("-password")
+
+    if(!data) {
+        return res.status(400).json({success: false, error: "Something went wrong."})
+    }
+    
+    res.json({ success: true, data, message: "User profile data fetched successfully." })
 })
 
 export default getProfileData;
