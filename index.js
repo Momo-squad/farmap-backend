0d76acd7341a1d2c00472d3e0ba09f06a937162e
yaman1337 <yaman.sarabariya@gmail.com>
2023-01-11 14:15:11 +0545

    add pubsub

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import { WebPubSubServiceClient } from "@azure/web-pubsub";
 
 import connectDB from "./utils/connectDB.js";
 
@@ -9,26 +10,39 @@ import sellerRouter from "./routes/seller.routes.js";
 import forumRouter from "./routes/forum.routes.js";
 
 dotenv.config();
-const { PORT, MONGO_URI } = process.env;
+const { PORT, MONGO_URI, PUBSUB_URI } = process.env;
 
 const app = express();
+const serviceClient = new WebPubSubServiceClient(PUBSUB_URI, "farmap");
 
 // database connection
-connectDB(MONGO_URI)
+connectDB(MONGO_URI);
 
 // middlewares
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // api routes
-app.use("/auth", authRouter)
-app.use("/user", userRouter)
-app.use("/seller", sellerRouter)
-app.use("/forum", forumRouter)
+app.use("/auth", authRouter);
+app.use("/user", userRouter);
+app.use("/seller", sellerRouter);
+app.use("/forum", forumRouter);
 
 app.get("/", (req, res) => {
-    res.send("Farmap backend service.")
-})
+  res.send("Farmap backend service.");
+});
+
+app.get("/negotiate", async (req, res) => {
+  let id = req.query.id;
+  if (!id) {
+    res.status(400).send("missing user id");
+    return;
+  }
+  let token = await serviceClient.getClientAccessToken({ userId: id });
+  res.json({
+    url: token.url,
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Farmap backend service is listening on port ${PORT}.`);
