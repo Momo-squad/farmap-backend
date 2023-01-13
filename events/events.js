f3f06e76275e6f0b0d724c23917da187e02a753f
yaman1337 <yaman.sarabariya@gmail.com>
2023-01-13 14:25:50 +0545

    add new notifications to database

---
+++ events/events.js
@@ -0,0 +1,22 @@
+import eventTypes from "../eventTypes.js";
+import EventEmitter from "node:events";
+import Notification from "../models/Notification.js";
+
+const farmapEvents = new EventEmitter();
+
+// save notification to db when someone follows a user ..
+farmapEvents.on(eventTypes.followed, async (data) => {
+  try {
+    let res = data.data;
+
+    let text = `${res.followed_by} followed you.`;
+
+    const newNotification = new Notification({ text, user_id: res.user_id });
+
+    await newNotification.save();
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+export default farmapEvents;
